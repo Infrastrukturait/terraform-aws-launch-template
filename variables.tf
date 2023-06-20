3ecdfc1beda3e874527411b154305650be9ed5fa
Rafał Masiarek <rafal@masiarek.pl>
2023-06-20 22:53:23 +0200

    chore: release `v0.1.0`

--- variables.tf
+++ variables.tf
@@ -1,1 +1,223 @@
-# variables.tf
+
+variable "name" {
+  description = "Name of launch template to be created"
+  type        = string
+  default     = ""
+}
+
+variable "use_name_prefix" {
+  description = "Determines whether to use `launch_template_name` as is or create a unique name beginning with the `launch_template_name` as the prefix"
+  type        = bool
+  default     = true
+}
+
+variable "description" {
+  description = "Description of the launch template"
+  type        = string
+  default     = null
+}
+
+variable "ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized"
+  type        = bool
+  default     = null
+}
+
+variable "image_id" {
+  description = "The AMI from which to launch the instance"
+  type        = string
+  default     = ""
+}
+
+variable "key_name" {
+  description = "The key name that should be used for the instance"
+  type        = string
+  default     = null
+}
+
+variable "user_data" {
+  description = "The Base64-encoded user data to provide when launching the instance"
+  type        = string
+  default     = null
+}
+
+variable "network_interfaces" {
+  description = "Customize network interfaces to be attached at instance boot time"
+  type        = list(any)
+  default     = []
+}
+
+variable "security_groups" {
+  description = "A list of security group IDs to associate"
+  type        = list(string)
+  default     = []
+}
+
+variable "instance_type" {
+  description = "The type of the instance. If present then `instance_requirements` cannot be present"
+  type        = string
+  default     = null
+}
+
+variable "instance_requirements" {
+  description = "The attribute requirements for the type of instance. If present then `instance_type` cannot be present"
+  type        = any
+  default     = {}
+}
+
+variable "metadata_options" {
+  description = "Customize the metadata options for the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "enable_monitoring" {
+  description = "Enables/disables detailed monitoring"
+  type        = bool
+  default     = true
+}
+
+variable "default_version" {
+  description = "Default Version of the launch template"
+  type        = string
+  default     = null
+}
+
+variable "update_default_version" {
+  description = "Whether to update Default Version each update. Conflicts with `default_version`"
+  type        = string
+  default     = null
+}
+
+variable "disable_api_termination" {
+  description = "If true, enables EC2 instance termination protection"
+  type        = bool
+  default     = null
+}
+
+variable "disable_api_stop" {
+  description = "If true, enables EC2 instance stop protection"
+  type        = bool
+  default     = null
+}
+
+variable "instance_initiated_shutdown_behavior" {
+  description = "Shutdown behavior for the instance. Can be `stop` or `terminate`. (Default: `stop`)"
+  type        = string
+  default     = null
+}
+
+variable "kernel_id" {
+  description = "The kernel ID"
+  type        = string
+  default     = null
+}
+
+variable "ram_disk_id" {
+  description = "The ID of the ram disk"
+  type        = string
+  default     = null
+}
+
+variable "block_device_mappings" {
+  description = "Specify volumes to attach to the instance besides the volumes specified by the AMI"
+  type        = list(any)
+  default     = []
+}
+
+variable "capacity_reservation_specification" {
+  description = "Targeting for EC2 capacity reservations"
+  type        = any
+  default     = {}
+}
+
+variable "cpu_options" {
+  description = "The CPU options for the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "credit_specification" {
+  description = "Customize the credit specification of the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "elastic_gpu_specifications" {
+  description = "The elastic GPU to attach to the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "elastic_inference_accelerator" {
+  description = "Configuration block containing an Elastic Inference Accelerator to attach to the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "enclave_options" {
+  description = "Enable Nitro Enclaves on launched instances"
+  type        = map(string)
+  default     = {}
+}
+
+variable "hibernation_options" {
+  description = "The hibernation options for the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "instance_market_options" {
+  description = "The market (purchasing) option for the instance"
+  type        = any
+  default     = {}
+}
+
+variable "license_specifications" {
+  description = "A list of license specifications to associate with"
+  type        = map(string)
+  default     = {}
+}
+
+variable "maintenance_options" {
+  description = "The maintenance options for the instance"
+  type        = any
+  default     = {}
+}
+
+variable "placement" {
+  description = "The placement of the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "private_dns_name_options" {
+  description = "The options for the instance hostname. The default values are inherited from the subnet"
+  type        = map(string)
+  default     = {}
+}
+
+
+variable "iam_instance_profile_arn" {
+  description = "Amazon Resource Name (ARN) of an existing IAM instance profile. Used when `create_iam_instance_profile` = `false`"
+  type        = string
+  default     = null
+}
+
+variable "iam_instance_profile_name" {
+  description = "The name of the IAM instance profile to be created (`create_iam_instance_profile` = `true`) or existing (`create_iam_instance_profile` = `false`)"
+  type        = string
+  default     = null
+}
+
+variable "tag_specifications" {
+  description = "The tags to apply to the resources during launch"
+  type        = list(any)
+  default     = []
+}
+
+variable "tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+  default     = {}
+}
